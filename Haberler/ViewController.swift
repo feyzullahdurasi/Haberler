2b67b014a4f4f7093fc32562530ee21fef338d58
Feyzullah Durası <durasifeyzullah672@gmail.com>
2024-05-16 16:35:49 +0300

    Initial Commit

---
+++ Haberler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Haberler
+//
+//  Created by Feyzullah Durası on 16.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
